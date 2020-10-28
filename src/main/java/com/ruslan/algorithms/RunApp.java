506edd18ebb23c028f18c8f1a2923cc74716d02f
Ruslan Urunov <urunov.ruslan@yandex.ru>
2020-10-28 09:24:45 +0300

    Исправил вызов метода, так как он статический.

--- src/main/java/com/ruslan/algorithms/RunApp.java
+++ src/main/java/com/ruslan/algorithms/RunApp.java
@@ -8,8 +8,7 @@ public class RunApp {
     public static void main(String[] args) {
 //        int[] nums = new int[] {0,1,0,3,12};
         int[] nums = new int[]{0, 1, 0, 3, 12, 0, 5, 0, 2}; // пробный, для большего кол-ва
-        MoveZeroes mvZeroes = new MoveZeroes();
-        mvZeroes.moveZeroes(nums);
+        MoveZeroes.moveZeroes(nums);
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         System.out.println(Arrays.toString(nums));
     }
